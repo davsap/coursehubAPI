5fa53a5b84515c91c88574de809775d5ae8d9048
davsap <david.parto@hotmail.com>
2018-09-13 23:18:09 +0200

    project structure

---
+++ src/main/java/fr/coursehub/CourseHubApi.java
@@ -0,0 +1,12 @@
+package fr.coursehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseHubApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseHubApi.class, args);
+	}
+}
